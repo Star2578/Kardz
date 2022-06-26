487db59c312985f49d0856766a83de0f7efa728b
Star2578 <nuttykingger@gmail.com>
2022-06-26 21:44:14 +0700

    Start Implementing enum card info

--- Assets/Script/CardData/Card.cs
+++ Assets/Script/CardData/Card.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum CardType : byte { CHAMPION, MINION, SPELL }
+public enum Region : byte { DEMACIA, PILTOVER, NOXUS, IONIA, BANDLE, BILGEWATER, FRELJORD, TARGON, SHURIMA, SHADOWISLE}
+public enum CreatureType : byte { NONE, YORDLES, SEAMONSTER, BEAST, DRAGON, MECH}
+
 [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class Card : ScriptableObject
 {
-    public LayerMask typeOfCard;
+    public List<CardType> cardTypes;
+    public List<Region> regions;
+    public List<CreatureType> creatureTypes;
     public int id;
     public string cardName;
     public int cost;
