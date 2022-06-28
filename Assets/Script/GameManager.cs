8aa8e1bb9319b804fa62cd5e6def4e9107ccb572
Star2578 <nuttykingger@gmail.com>
2022-06-28 15:07:38 +0700

    Sorting items in GameManager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -11,24 +11,34 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
 
+    [Header("Mana")]
     public int currMana;
     public int currMaxMana = 0;
     public int maximumMana = 10;
     
+    [Header("Health")]
+    public int currHealth;
+    public int maxHealth = 40;
+
+    [Header("Deck")]
+    public int deckSize = 40;
+    public int handSize = 8;
+    public int cardInHand;
+
+    [Header("Calls")]
     public Card placeholder;
     public GameObject cardPf;
     public GameObject cardSummonedPf;
-
     public Canvas maincanva;
     public RectTransform dropZone;
 
-    public int cardInHand;
-    public bool canLook = true;
-
+    [Header("Target")]
     public List<Collider2D> targetContainer = new List<Collider2D>();
     public GameObject targetPointer;
-    public bool isActive;
 
+    [Header("Graveyard")]
     public List<Card> graveyardPlayer = new List<Card>();
     public List<Card> graveyardEnemy = new List<Card>();
+    [HideInInspector] public bool isActive;
+    [HideInInspector] public bool canLook = true;
 }
